e1753950270b3e921d144b0825f095b0458b4172
MrMazza4 <163433144+MrMazza4@users.noreply.github.com>
2024-10-07 07:48:35 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futebol',
+    'O que é um cartão vermelho?',
+    'O cartão vermelho é uma punição após uma falta grave ou dois amarelos, o jogador tem que sair do campo e o time jogar com 1 a menos, e o jogador não pode entrar na proxima partida'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil',
+    'A capital do Brasil é Brasília'
+)
+
+criaCartao(
+    'Futebol',
+    'Quem é o maior jogador da história?',
+    'O maior jogador da história do futebol é o Pelé, que conquistou simplismente 3 copas do mundo e outros diversos títulos.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz bom dia em INGLÊS',
+    'Bom dia em inglês é Good Morning.'
+)
